5fd0c7800eb8aa2b0bbb8a33c7aecee1c66cf5aa
giraydorukyurt7 <giraysoftwareengineer3.14@hotmail.com>
2025-01-03 18:08:19 +0300

    text manipulation with alphabet

--- internal_functions/project_functions.py
+++ internal_functions/project_functions.py
@@ -1,8 +1,23 @@
 import numpy as np
 from PIL import Image
 import cv2 as cv
+import string
 
-img = cv.imread("test_image.jpg")
+UpperCases = list(string.ascii_uppercase)
+
+img = cv.imread("cat.png")
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-print(gray)
+
+gray = np.floor(gray / 10).astype(int)
+
+gray_obj = gray.astype(object)
+gray_obj[gray == 25] = "A"
+
+
+for i in range(0,25):
+    gray_obj[gray == i] = UpperCases[i]
+
+with open("manipulated_test.txt", "w") as f:
+    for row in gray_obj:
+        f.write(" ".join(map(str, row)) + "\n")
